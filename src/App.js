b2f8d7f3623871a3389c2a02e51ea090230009b1
Yash <yashgarg0905@gmail.com>
2021-07-16 01:27:40 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+//Global Styled
+import GlobalStyle from "./Components/GlobalStyled";
+
+//Pages
 import AboutUs from "./Pages/AboutUs";
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
